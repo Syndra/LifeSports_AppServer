2513c4698a500186690f7b95490d34f9f1daa71f
lenny-kong <forlennykong@gmail.com>
2019-05-27 15:09:47 +0900

    asdf

--- nodejs/schedule/schedule.js
+++ nodejs/schedule/schedule.js
@@ -100,7 +100,7 @@ exports.matchingStatus = function (request, response)
     console.log('Data : ', data);
     var connection = mysqlLoader.mysql_load();
     connection.query(
-      'SELECT gym_ID, gym_name, fac_ID, fac_name ,starttime, endtime, schedule_ID, max_participant, min_participant, cur_participant, TO_DAYS(sysdate()) - TO_DAYS(starttime) as dday from fac_schedule NATURAL join gym NATURAL join fac_info where schedule_ID in (SELECT reserv_ID as schedule_ID from open_matches where UDID = ?) order by starttime desc',
+      'SELECT gym_ID, gym_name, fac_ID, fac_name ,starttime, endtime, schedule_ID, max_participant, min_participant, cur_participant, TO_DAYS(sysdate()) - TO_DAYS(starttime) as dday from schedule_detail where schedule_ID in (SELECT reserv_ID as schedule_ID from open_matches where UDID = ?) order by starttime desc limit 5',
     [data.UDID],
     function(err, results){
       if(err)
