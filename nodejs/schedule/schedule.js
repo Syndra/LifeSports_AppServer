a1b1d4d537180b6947844f336fd9b9e695bc35c7
Lenny-Kong <dadasa13@naver.com>
2019-05-30 20:06:44 +0900

    asd

--- nodejs/schedule/schedule.js
+++ nodejs/schedule/schedule.js
@@ -150,7 +150,7 @@ exports.reservationTypeSearch = function (request, response)
   {
     data = JSON.parse(Buffer.concat(chunks).toString());
     var connection = mysqlLoader.mysql_load();
-    _searchDate = data.starttime;
+    _searchDate = new Date(data.starttime);
     var _endDate = new Date();
     _endDate.setDate(_searchDate.getDate() + 7)
     searchDate = _searchDate.getFullYear() + '-' + _searchDate.getMonth() + '-' + _searchDate.getDay();
