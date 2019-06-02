9e918a1a1ffe770b4ceed642608f3ce225895a0c
Lenny-Kong <dadasa13@naver.com>
2019-06-02 18:21:43 +0900

    asdasd

--- nodejs/user/user.js
+++ nodejs/user/user.js
@@ -10,53 +10,49 @@ var ejs = require('ejs');
 var mysqlLoader = require("../database/mysqlLoader.js")
 
 //Register New User
-exports.regiUser = function (request, response)
-{
+exports.regiUser = function (request, response) {
   var body = '';
   const chunks = [];
   var _data;
   request.on('data', chunk => chunks.push(chunk));
-  request.on('end', () =>
-  {
+  request.on('end', () => {
     data = JSON.parse(Buffer.concat(chunks).toString());
     console.log('Data : ', data);
     var connection = mysqlLoader.mysql_load();
     connection.query(
       'INSERT INTO `user` (ID, PWD, name, gender, ) VALUES (?, ?, ?, ?, ?)',
-    [data.ID, data.PWD, data.name, data.gender, data.birth],
-    function(err, results){
-      if(err)
-        console.log(err);
-      else{
-        response.send("Success");
-      }
-    });
+      [data.ID, data.PWD, data.name, data.gender, data.birth],
+      function (err, results) {
+        if (err)
+          console.log(err);
+        else {
+          response.send("Success");
+        }
+      });
   });
 
 }
 
 //Try to login
-exports.loginTry = function (request, response)
-{
+exports.loginTry = function (request, response) {
   var body = '';
   const chunks = [];
   var _data;
   request.on('data', chunk => chunks.push(chunk));
-  request.on('end', () =>
-  {
+  request.on('end', () => {
     data = JSON.parse(Buffer.concat(chunks).toString());
     console.log('Data : ', data);
     var connection = mysqlLoader.mysql_load();
     connection.query(
       'SELECT UDID, ID, name, gender, birth FROM `user` WHERE ID=? AND PWD=?',
-    [data.ID, data.PWD],
-    function(err, results){
-      if(err)
-        console.log(err);
-      else{
-        response.send(results);
-      }
-    });
+      [data.ID, data.PWD],
+      function (err, results) {
+        if (err)
+          console.log(err);
+        else {
+          response.send(results);
+        }
+      });
   });
 
 }
@@ -73,7 +69,7 @@ exports.getUserInfo = function (request, response) {
     console.log('Data : ', data);
     var connection = mysqlLoader.mysql_load();
     connection.query(
-      'SELECT ID, name, gender, birth, profile_fig FROM `user` WHERE UDID = ?',
+      'SELECT ID, name, gender, birth, profile_fig, mvp_point FROM `user` natural join soccer_record WHERE UDID = ?',
       [data.UDID],
       function (err, result) {
         if (err)
@@ -164,8 +160,74 @@ exports.getUserInfo = function (request, response) {
                                         console.log(err);
                                       else {
                                         result[0].month_4 = results[0].month_4;
-                                        console.log(result);
-                                        response.send(result);
+                                        connection.query(
+                                          "select count(*) as ranking " +
+                                          "from soccer_record " +
+                                          "where MMR > (select MMR from soccer_record where UDID = ?)",
+                                          [data.UDID], function (err, results) {
+                                            if (err)
+                                              console.log(err);
+                                            else {
+                                              result[0].ranking = results[0].ranking;
+                                              connection.query(
+                                                "select UDID, MMR, name, profile_fig "+
+                                                "from soccer_record natural join `user` "+
+                                                "where MMR > (select MMR from soccer_record where UDID = ?) "+
+                                                "order by MMR "+
+                                                "limit 2",
+                                                [data.UDID], function (err, results) {
+                                                  if (err)
+                                                    console.log(err);
+                                                  else {
+                                                    result[0].upper_user = results;
+                                                    connection.query(
+                                                      "select UDID, MMR, name, profile_fig "+
+                                                      "from soccer_record natural join `user` "+
+                                                      "where MMR < (select MMR from soccer_record where UDID = ?) "+
+                                                      "order by MMR desc "+
+                                                      "limit 2",
+                                                      [data.UDID], function (err, results) {
+                                                        if (err)
+                                                          console.log(err);
+                                                        else {
+                                                          result[0].under_user = results;
+                                                          connection.query(
+                                                            "select count(*) as total_match "+
+                                                            "from match_participant "+
+                                                            "where UDID = ?",
+                                                            [data.UDID], function (err, results) {
+                                                              if (err)
+                                                                console.log(err);
+                                                              else {
+                                                                result[0].total_match = results[0].total_match;
+                                                                connection.query(
+                                                                  "select (sum(is_win) / count(*)) as recent_winning_rate from "+
+                                                                  "( "+
+                                                                  "select * "+
+                                                                  "from fac_schedule natural join  "+
+                                                                  "( "+
+                                                                  "select match_ID as schedule_ID, UDID, is_win from match_participant where UDID = ? "+
+                                                                  ") as b "+
+                                                                  "order by starttime desc "+
+                                                                  "limit 10 "+
+                                                                  ") as c",
+                                                                  [data.UDID], function (err, results) {
+                                                                    if (err)
+                                                                      console.log(err);
+                                                                    else {
+                                                                      result[0].recent_winning_rate = results[0].recent_winning_rate;
+                                                                      console.log(result);
+                                                                      response.send(result);
+                                                                    }
+                                                                  });
+                                                              }
+                                                            });
+                                                        }
+                                                      });
+                                                  }
+                                                });
+                                            }
+                                          });
                                       }
                                     });
                                 }
@@ -184,59 +246,55 @@ exports.getUserInfo = function (request, response) {
 }
 
 //중복검사
-exports.checkIdDup = function (request, response)
-{
+exports.checkIdDup = function (request, response) {
   var body = '';
   const chunks = [];
   var _data;
   request.on('data', chunk => chunks.push(chunk));
-  request.on('end', () =>
-  {
+  request.on('end', () => {
     data = JSON.parse(Buffer.concat(chunks).toString());
     console.log('Data : ', data);
     var connection = mysqlLoader.mysql_load();
     connection.query(
       'SELECT COUNT(*) as isduplicated FROM `user` WHERE ID = ?',
-    [data.ID],
-    function(err, results){
-      if(err)
-        console.log(err);
-      else{
-        response.send(results);
-      }
-    });
+      [data.ID],
+      function (err, results) {
+        if (err)
+          console.log(err);
+        else {
+          response.send(results);
+        }
+      });
   });
 
 }
 
 
 //유저가 가입한 팀 목록 조회
-exports.searchUserTeam = function (request, response)
-{
+exports.searchUserTeam = function (request, response) {
   var body = '';
   const chunks = [];
   var _data;
   request.on('data', chunk => chunks.push(chunk));
-  request.on('end', () =>
-  {
+  request.on('end', () => {
     data = JSON.parse(Buffer.concat(chunks).toString());
     console.log('Data : ', data);
     var connection = mysqlLoader.mysql_load();
     connection.query(
       "SELECT team_ID, team_name, team_leader_UDID, team_MMR, team_main_subj, winning_rate, if(team_leader_UDID = ?, '1', '0') as isleader from team natural join team_user_list where UDID = ?",
-    [data.UDID, data.UDID],
-    function(err, results){
-      if(err)
-        console.log(err);
-      else{
-        response.send(results);
-      }
-    });
+      [data.UDID, data.UDID],
+      function (err, results) {
+        if (err)
+          console.log(err);
+        else {
+          response.send(results);
+        }
+      });
   });
 
 }
 
-function get_month_record(connection, data){
+function get_month_record(connection, data) {
 
-  
+
 }
