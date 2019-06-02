8f9ee5a4af40aa67bb45441750229d680f097d65
Lenny-Kong <dadasa13@naver.com>
2019-06-03 01:57:21 +0900

    asdsad

--- nodejs/schedule/schedule.js
+++ nodejs/schedule/schedule.js
@@ -365,7 +365,7 @@ exports.joinMatching = function (request, response)
             for(var i = 0; i < results.length; i++){
               connection.query(
                 "UPDATE open_match_team SET is_team_A = "+
-                "?"+
+                "? "+
                 "WHERE match_ID = ? AND UDID = ?",
                  [results[i].is_team_A, data.schedule_ID, results[i].UDID], 
               function(err, results_){
