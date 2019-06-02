05893392cd1b9e870e73d51f55e650e2343ceefd
Lenny-Kong <dadasa13@naver.com>
2019-06-03 08:28:45 +0900

    asdasd

--- nodejs/schedule/schedule.js
+++ nodejs/schedule/schedule.js
@@ -81,7 +81,7 @@ exports.reservationStatusTEST = function (request, response)
     console.log('Data : ', data);
     var connection = mysqlLoader.mysql_load();
     connection.query(
-      'SELECT gym_ID, gym_name, fac_ID, fac_name ,starttime, endtime, schedule_ID, TO_DAYS(sysdate()) - TO_DAYS(starttime) as dday from fac_schedule natural join gym NATURAL join fac_info where schedule_ID in (SELECT reserv_ID from reserv_matches WHERE reserv_team_ID = ( SELECT team_ID from team_user_list where UDID = ?)) LIMIT 2',
+      'SELECT gym_ID, gym_name, fac_ID, fac_name ,starttime, endtime, schedule_ID, TO_DAYS(sysdate()) - TO_DAYS(starttime) as dday from fac_schedule natural join gym NATURAL join fac_info where schedule_ID in (SELECT reserv_ID from reserv_matches WHERE reserv_team_ID = ( SELECT team_ID from team_user_list where UDID = ?)) WHERE endtime > sysdate() LIMIT 2',
     [data.UDID],
     function(err, results){
       if(err)
