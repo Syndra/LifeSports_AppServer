192428f158b19c944321c316e7a9ebaf7ed5f78f
Lenny-Kong <dadasa13@naver.com>
2019-06-02 17:48:23 +0900

    asd

--- nodejs/user/user.js
+++ nodejs/user/user.js
@@ -74,7 +74,8 @@ exports.getUserInfo = function (request, response)
     data = JSON.parse(Buffer.concat(chunks).toString());
     console.log('Data : ', data);
     var connection = mysqlLoader.mysql_load();
-    console.log("hello", get_month_record(connection, data).month_0);
+    var month_record = get_month_record(connection, data);
+    console.log("hello", month_record);
     connection.query(
       'SELECT ID, name, gender, birth, profile_fig FROM `user` WHERE UDID = ?',
     [data.UDID],
